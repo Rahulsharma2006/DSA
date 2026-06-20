e8a9a4fdd868e56d5ff13767525e66dd7e773066
Rahul Sharma <rs0987jrkg@gmail.com>
2026-06-20 15:10:54 +0530

    Time: 95 ms (24.11%), Space: 40.6 MB (99.75%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>>adj(n+1);
-        for(int i =0;i<times.size();i++){
-            int sr = times[i][0];
-             int dest = times[i][1];
-              int w = times[i][2];
-              adj[sr].push_back({dest,w});
-        }
-        vector<int>dist(n+1,INT_MAX);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        dist[k]=0;
-        pq.push({0,k});
-        while(!pq.empty()){
-            pair<int,int>p = pq.top();
-            pq.pop();
-              int d = p.first;
-              int node = p.second;
-              if(d>dist[node])continue;
+        vector<int>res(n+1,INT_MAX);
+        res[k] =0;
 
-              for(int i =0;i<adj[node].size();i++){
-                int neigh = adj[node][i].first;
-                int wt = adj[node][i].second;
-                if(d+wt<dist[neigh]){
-                    dist[neigh] = d+wt;
-                    pq.push({d+wt,neigh});
+        for(int i =0;i<n-1;i++){
+            for(int j =0;j<times.size();j++){
+                int sr = times[j][0];
+                int d = times[j][1];
+                int wt = times[j][2];
+                if(res[sr]!=INT_MAX && res[d]>res[sr]+wt){
+                    res[d]=res[sr]+wt;
                 }
-              }
+            }
         }
-       int res = dist.empty() ? -1 : *max_element(dist.begin()+1, dist.end());
-       if(res==INT_MAX){
-        return -1;
-       }
-        return res;
+        
+        if(*max_element(res.begin()+1,res.end())==INT_MAX)return -1;
+        return *max_element(res.begin()+1,res.end());
     }
+   
 };
