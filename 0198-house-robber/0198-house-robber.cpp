3cd8664b515738dcad8a89bfb82987f53d51aba8
Rahul Sharma <rs0987jrkg@gmail.com>
2026-06-30 14:33:27 +0530

    Time: 0 ms (100%), Space: 10.2 MB (77.93%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-      int maxMoney(vector<int>& a, int n , int i ,int free, vector<vector<int>>&Dp){
-
-         if(i==n) return 0;
-
-        if(Dp[i][free]!=-1) return Dp[i][free];
-
-        if(free==0)return Dp[i][free]=  maxMoney(a,n,i+1,1,Dp);
-
-        int c1 = a[i] + maxMoney(a,n,i+1,0,Dp);
-        int c2 = maxMoney(a,n,i+1,1,Dp);
-
-        return Dp[i][free]= max(c1,c2);
-      }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-          vector<vector<int>>Dp(n);
-          for(int i =0;i<n;i++){
-            vector<int>t(2,-1);
-            Dp[i]=t;
-          }
-         return maxMoney(nums,n,0,1,Dp);
+         int n = nums.size();
+         int prev_prev=0;
+         int prev=nums[n-1];
+         if(nums.size()==0) return 0;
+         if(nums.size()==1) return nums[0];
+         int ans =0;
+    for(int i=n-2;i>=0;i--){
+       ans = max(nums[i]+prev_prev,prev);
+       prev_prev=prev;
+       prev=ans;
+    }
+    return ans;
     }
 };
