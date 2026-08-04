f4ce60a565771713d0f2f477968ab0222492c116
Rahul Sharma <rs0987jrkg@gmail.com>
2026-08-04 21:05:16 +0530

    Time: 14 ms (6.06%), Space: 34.4 MB (5.13%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int>ans;
-        for(int i =1;i<nums.size();i++){
-            if(abs(nums[i]-nums[i-1])>1){
-                for(int k = nums[i-1]+1;k<nums[i];k++){
-                    ans.push_back(k);
-                }
-            }
-        }
-        return ans;
+          //Approach 1
+
+        // sort(nums.begin(),nums.end());
+        // vector<int>ans;
+        // for(int i =1;i<nums.size();i++){
+        //     if(abs(nums[i]-nums[i-1])>1){
+        //         for(int k = nums[i-1]+1;k<nums[i];k++){
+        //             ans.push_back(k);
+        //         }
+        //     }
+        // }
+        // return ans;
+       
+         //Approach 2
+         unordered_map<int,int>mp;
+         vector<int>ans;
+         int high =  *max_element(nums.begin(),nums.end());
+         int low =  *min_element(nums.begin(),nums.end());
+           for(int i =low;i<high;i++){
+            mp[i]++;
+           }
+           for (int x : nums)
+          mp.erase(x);
+
+ 
+         for (auto &it : mp)
+         ans.push_back(it.first);
+
+        sort(ans.begin(), ans.end());
+
+          return ans;
     }
 };
